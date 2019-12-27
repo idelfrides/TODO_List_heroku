ab117240d6dd1a9236f0ceaca8a6aac3344f315d
idelfrides <idelfridesjorgepapai@gmail.com>
2019-12-27 01:51:25 -0300

    Configure project to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
+
 app.config.from_object('config')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
@@ -16,6 +20,10 @@ manager.add_command('db', MigrateCommand)
 
 login_manager = LoginManager(app)
 
+'''
+def getDB():
+    return db
+'''
 def getApp():
     return app
 
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-DEBUG = False
+'''
+DEBUG = True
 
 import os.path
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -6,5 +7,17 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'todolist_db.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = b'\xdd\x8b\xad\x13\xf8\xda-\xfd\xa2c\\B\x1a\xed4\xb1'
+'''
+# ------------------------------------------------------
 
-# SECRET_KEY =  b'\xdd\x8b\xad\x13\xf8\xda-\xfd\xa2c\\B\x1a\xed4\xb1'
+DEBUG = False
+
+import os.path
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'todolist_db.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# SECRET_KEY = b'\xdd\x8b\xad\x13\xf8\xda-\xfd\xa2c\\B\x1a\xed4\xb1'
+
+# heroku config:set PYTHONPATH=TODO_List_heroku
